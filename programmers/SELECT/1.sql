73fc685c6792c1bdb0eae5b1f60e7ba1328d98f6
JJIMINSHIN <scalla1031@gmail.com>
2023-03-13 16:06:04 +0900

    3월에 태어난 여성 회원 목록 출력하기

---
+++ programmers/SELECT/1.sql
@@ -0,0 +1,9 @@
+-- 3월에 태어난 여성 회원 목록 출력하기
+
+SELECT member_id, member_name, gender, 
+DATE_FORMAT(date_of_birth, '%Y-%m-%d') AS date_of_birth
+FROM MEMBER_PROFILE
+WHERE MONTH(date_of_birth) =3
+AND gender = 'W'
+AND tlno IS NOT NULL
+;
