d0d79853d4ba1924eef0479746d425d88f098da0
JJIMINSHIN <scalla1031@gmail.com>
2023-04-11 20:52:14 +0900

    FIX : merge branch

--- 200제/001.sql
+++
@@ -1,4 +0,0 @@
--- 사원 테이블에서 사원 번호와 이름과 월급을 출력해 보세요
-
-SELECT empno, ename, sal 
-FROM EMP;
--- 200제/002.sql
+++
@@ -1,7 +0,0 @@
--- 사원 테이블에서 모든 열 출력하기
-
-SELECT * FROM EMP;
-
--- 모든 컬럼 출력하고 맨 끝에 특정 컬럼 추가하기
-
-SELECT EMP.* , JOB FROM EMP;
